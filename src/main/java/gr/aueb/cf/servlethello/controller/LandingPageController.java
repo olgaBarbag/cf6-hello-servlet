a6ed1c3a51f099e191870737ed8c8805e3d6a2f5
Olga B. <olbarbag@gmail.com>
2024-09-08 13:32:22 +0300

    creates /home endpoint

---
+++ src/main/java/gr/aueb/cf/servlethello/controller/LandingPageController.java
@@ -0,0 +1,24 @@
+package gr.aueb.cf.servlethello.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.Serial;
+
+@WebServlet("/home")
+public class LandingPageController extends HttpServlet {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html; charset=UTF-8");
+        //return index.jsp
+        request.getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(request, response);
+    }
+}
